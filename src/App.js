bd5413c2664fedc61e35fa51fe9f7336607d822c
Jesse Sandvik <sandvikjesse@gmail.com>
2022-03-14 01:01:59 -0400

    buttons added to main page

--- src/App.js
+++ src/App.js
@@ -10,8 +10,20 @@ function App() {
       <div className="layout">
         <Header />
         <Main>
-          <div className="item b"></div>
+          <div className="item b">
+            <div className="intro">
+              <h1>Jesse Sandvik</h1>
+              <h2>Software Developer</h2>
+            </div>
+          </div>
+          <div className="item c">
+            <span className="btns">
+              <button className="btn-port">View My Portfolio</button>
+              <button className="btn-cont">contact Me</button>
+            </span>
+          </div>
           <div className="item e"></div>
+          <div className="item f"></div>
         </Main>
         <Footer />
       </div>
